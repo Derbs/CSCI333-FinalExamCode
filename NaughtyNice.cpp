d459ba26574d8832fb8ffe2c11fc5ad868934d2a
Derbs <derbs@debian.mh.rockynet.wireless.unca>
2012-12-07 02:05:05 -0500

    Full functional.  No seg faults.

--- NaughtyNice.cpp
+++ NaughtyNice.cpp
@@ -7,23 +7,21 @@ using std::string;
 
 int main(int argc, char** argv) {
   if(argc == 4) {
-
-    std::cout<<"No sef fault yet"<<std::endl;
     int threshold;
     std::string threshRaw (argv[3]);
-    std::cout<<"No sef fault yet"<<std::endl;
     std::stringstream converter (threshRaw);
     if(!(converter >> threshold)) threshold = 0;
-    std::cout<<"No sef fault yet"<<std::endl;
+    std::cout<<"The threshold is "<<threshold<<std::endl;
     std::ifstream reader (argv[2]);
-    std::cout<<"No sef fault yet"<<std::endl;
     std::map<std::string,int> names;
+    std::string curName;
+    int oldScore;
     while(reader.good()) {
-      std::string curName ( (char*)reader.get());
-      if(names.count(curName)) {
-        std::pair <string,int> nxt (curName,names[curName]);
-        nxt.second += 1;
-	names.insert(nxt);
+      curName.clear(); 
+      std::getline(reader, curName);
+      if(names.count(curName)!=0) {
+        oldScore = names[curName];
+	names[curName] = oldScore + 1;
       }
       else {
 	std::pair <string,int> nxt (curName,1);
@@ -31,16 +29,18 @@ int main(int argc, char** argv) {
       }
     }
     reader.close();
-    
-    std::cout<<"No sef fault yet"<<std::endl;
     std::map<std::string,int>::iterator it;
     std::cout<<"Nice Folk:"<<std::endl;
     for(it = names.begin(); it != names.end(); it++) {
-      if(it->second>threshold) std::cout<<it->first<<std::endl;
+      if(it->second>threshold) {
+        std::cout<<it->first<<std::endl;
+      }
     }
-    std::cout<<"Naughty Individuals:"<<std::endl;
+    std::cout<<std::endl<<"Naughty Individuals:"<<std::endl;
     for(it = names.begin(); it != names.end(); it++) {
-      if(it->second<=threshold) std::cout<<it->first<<std::endl;
+      if(it->second<=threshold) {
+        std::cout<<it->first<<std::endl;
+      }
     }
   }
   return 0;
